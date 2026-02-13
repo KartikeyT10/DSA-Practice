0f420e306e12b2a556962c4c86af2283c380e3d5
Kartikey Tiwari <98750470+KartikeyT10@users.noreply.github.com>
2026-02-13 23:21:34 +0530

    Create Minimum_Number_of_arrows.cpp

---
+++ Arrays/Minimum_Number_of_arrows.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        
+        if (points.empty()) return 0;
+
+        sort(points.begin(), points.end(), [](auto &a, auto &b) {
+            return a[1] < b[1];
+        });
+
+        int arrows = 1;            
+        int currentEnd = points[0][1];
+
+        for (int i = 1; i < points.size(); i++) {
+
+            if (points[i][0] > currentEnd) {
+                arrows++;               
+                currentEnd = points[i][1];
+            }
+        }
+
+        return arrows;
+    }
+};
