b58e8bef95c7342cee8fb9a73d11f2329006e3b4
Kartikey Tiwari <98750470+KartikeyT10@users.noreply.github.com>
2026-02-14 11:42:22 +0530

    Create Longest_Continuous_Increasing_Subsequence.cpp

---
+++ Arrays/Longest_Continuous_Increasing_Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        int longest = 1;      
+        int currentLen = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            if (nums[i] > nums[i - 1]) {
+                currentLen++;
+            } else {
+                currentLen = 1;
+            }
+
+            longest = max(longest, currentLen);
+        }
+
+        return longest;
+    }
+};
