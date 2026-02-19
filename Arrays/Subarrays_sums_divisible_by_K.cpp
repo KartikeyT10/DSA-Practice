71a8ad4eb40e020ecf5741f88721ffcb6c3a9b1e
Kartikey Tiwari <98750470+KartikeyT10@users.noreply.github.com>
2026-02-19 22:40:28 +0530

    Create Subarrays_sums_divisible_by_K.cpp

---
+++ Arrays/Subarrays_sums_divisible_by_K.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> freq;
+        
+        freq[0] = 1; 
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int x : nums) {
+            prefixSum += x;
+
+            int remainder = prefixSum % k;
+
+            if (remainder < 0) remainder += k;
+
+            if (freq.find(remainder) != freq.end()) {
+                count += freq[remainder];
+            }
+
+            freq[remainder]++;
+        }
+
+        return count;
+    }
+};
