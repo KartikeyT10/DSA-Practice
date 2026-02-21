f474ef8657461dd6609ca7386333daa9fd7dca1f
Kartikey Tiwari <98750470+KartikeyT10@users.noreply.github.com>
2026-02-21 22:44:29 +0530

    Create Shortest_subarray.cpp

---
+++ Arrays/Shortest_subarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<long long> P(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            P[i + 1] = P[i] + nums[i];
+        }
+
+        deque<int> dq;
+        
+        int ans = INT_MAX;
+        
+        for (int i = 0; i <= n; i++) {
+
+            while (!dq.empty() && P[i] - P[dq.front()] >= k) {
+                ans = min(ans, i - dq.front());
+                dq.pop_front();
+            }
+
+            while (!dq.empty() && P[i] <= P[dq.back()]) {
+                dq.pop_back();
+            }
+            
+            dq.push_back(i);
+        }
+        
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
