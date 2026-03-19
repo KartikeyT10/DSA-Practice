a672977032ae0a03f8cd709e22872811e60cbf18
Kartikey Tiwari <98750470+KartikeyT10@users.noreply.github.com>
2026-03-19 20:47:27 +0530

    Create Maximum_Number_of_vowels.cpp

---
+++ Strings/Maximum_Number_of_vowels.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int count = 0, ans = 0;
+
+        for (int i = 0; i < k; i++) {
+            if (string("aeiou").find(s[i]) != string::npos)
+                count++;
+        }
+
+        ans = count;
+
+        for (int i = k; i < s.size(); i++) {
+            if (string("aeiou").find(s[i]) != string::npos)
+                count++;
+
+            if (string("aeiou").find(s[i - k]) != string::npos)
+                count--;
+
+            ans = max(ans, count);
+
+            if (ans == k) return k;
+        }
+
+        return ans;
+    }
+};
